9c668d373b0371625822874f492f811f36dbc778
Pmsilva1 <pmsilva1@gmail.com>
2022-03-18 00:28:20 +0000

    Added an embed writer from json file

--- saliva_bot.py
+++ saliva_bot.py
@@ -3,8 +3,23 @@ load_dotenv()
 
 import os
 token = os.environ.get("saliva_token")
-
+owner_role_id = int(os.environ.get("owner_role_id"))
 import discord
+
+import json
+
+#def make_embed(message):
+    #msg_json = json.loads(message)
+    #content = msg_json["content"]
+    #embed = msg_json["embed"]
+
+    #embedVar = discord.Embed(title=embed["title"], description=embed["description"], color=embed["color"])
+    #embedVar.set_author()
+    #embedVar.set_footer()
+    #embedVar.set_image()
+    #embedVar.set_thumbnail()
+    #embedVar.add_field(name="Field1", value="hi", inline=False)
+
 
 client = discord.Client()
 
@@ -17,7 +32,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    role = discord.utils.find(lambda r: r.id == owner_role_id, message.guild.roles)
 
+    if role in message.author.roles:
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+
+        if message.content.startswith('!!hello'):
+            with open('example.json', 'r') as help_file:
+                help_commands = json.load(help_file)
+                help_embed = discord.Embed.from_dict(help_commands)
+                await message.channel.send(embed=help_embed)
+
+        if message.content.startswith('!!embedmsg'):
+            await message.channel.send('Where do you want to send this embeded message?')
+            msg = await client.wait_for('message')
+            #await message.channel.send(f"You said this? {msg.content}")
+            test = json.loads(msg.content)
+            embedVar = discord.Embed.from_dict(test)
+            print(embedVar)
+            await message.channel.send(embed=embedVar)
 client.run(token)
